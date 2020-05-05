34b924323a2d822a3ea6fae56c796100ed4064d3
lukasz <slaskww@gmail.com>
2020-05-05 11:51:13 +0200

    Add validation to domain classes: Order and Pizza

--- src/main/java/pizza/domain/Order.java
+++ src/main/java/pizza/domain/Order.java
@@ -1,18 +1,31 @@
 package pizza.domain;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class Order {
 
     private String id;
+
+    @NotBlank(message = "Podaj imię i nazwisko")
     private String name;
+    @NotBlank(message = "Podaj ulicę dostawy")
     private String street;
+    @NotBlank(message = "Podaj nazwę miasta dostawy")
     private String city;
     private String state;
+    @NotBlank(message = "Podaj kod pocztowy dostawy")
     private String postCode;
+    @CreditCardNumber
     private String creditCardNo;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message = "Wartość musi być w formacie MM/RR")
     private String creditCardExp;
+    @Digits(integer = 3, fraction = 0, message = "Podaj poprawny numer kodu CVV")
     private String creditCardCvv;
 
 
--- src/main/java/pizza/domain/Pizza.java
+++ src/main/java/pizza/domain/Pizza.java
@@ -2,12 +2,17 @@ package pizza.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Pizza {
 
     private String id;
+    @NotNull
+    @Size(min = 5, message = "Nazwa powinna zawierać co najmniej 5 znaków")
     private String name;
+    @Size(min = 1, message = "Wybierz co najmniej jeden składnik")
     private List<String> ingredients;
 }
