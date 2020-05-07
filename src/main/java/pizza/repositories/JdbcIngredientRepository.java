8acc96e3b0e7b8c8162539d16f8056cfdba3e405
lukasz <slaskww@gmail.com>
2020-05-07 18:20:43 +0200

    Add schema.sql and data.sql files

--- src/main/java/pizza/repositories/JdbcIngredientRepository.java
+++ src/main/java/pizza/repositories/JdbcIngredientRepository.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import pizza.domain.Ingredient;
 
+
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+
+delete from Taco_Order_Tacos;
+delete from Taco_Ingredients;
+delete from Taco;
+delete from Taco_Order;
+delete from Ingredient;
+
+insert into Ingredient(id, name, type) values('DC', 'Corn', 'DOUGH');
+insert into Ingredient(id, name, type) values('CM', 'Mozarella', 'CHEESE');
+insert into Ingredient(id, name, type) values('DW', 'Wheat', 'DOUGH');
+insert into Ingredient(id, name, type) values('CC', 'Cheddar', 'CHEESE');
+insert into Ingredient(id, name, type) values('MC', 'Chicken', 'MEAT');
+insert into Ingredient(id, name, type) values('MF', 'Salmon', 'MEAT');
+insert into Ingredient(id, name, type) values('MB', 'Beef', 'MEAT');
+insert into Ingredient(id, name, type) values('VT', 'Tomato', 'VEGETABLE');
+insert into Ingredient(id, name, type) values('VB', 'Basil', 'VEGETABLE');
+insert into Ingredient(id, name, type) values('VP', 'Jalapeno', 'VEGETABLE');
+insert into Ingredient(id, name, type) values('VO', 'Onion', 'VEGETABLE');
+insert into Ingredient(id, name, type) values('VOR', 'Oregano', 'VEGETABLE');
+insert into Ingredient(id, name, type) values('VM', 'Mushrooms', 'VEGETABLE');
+insert into Ingredient(id, name, type) values('SG', 'Garlic sauce', 'SAUCE');
+insert into Ingredient(id, name, type) values('ST', 'Tomato sauce', 'SAUCE');
+insert into Ingredient(id, name, type) values('SB', 'Barbecue sauce', 'SAUCE');
