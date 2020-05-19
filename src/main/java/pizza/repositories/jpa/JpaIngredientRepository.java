aeb91bb021c399e166346d29de4ff6230f9f04d0
lukasz <slaskww@gmail.com>
2020-05-19 15:55:23 +0200

    Add some additional methods to JpaOrderRepository

--- src/main/java/pizza/repositories/jpa/JpaIngredientRepository.java
+++ src/main/java/pizza/repositories/jpa/JpaIngredientRepository.java
@@ -10,6 +10,16 @@ import pizza.domain.Ingredient;
  * Rozszerzając nasz interfejs o sparametryzowany interfejs JpaRepository otrzymujemy gotowy zestaw metod ogólnego przeznaczenia trwałego magazynu danych dla encji.
  * Po uruchomieniu aplikacji Spring Data JPA samodzielnie generuje potrzebne implementacje.
  *
+ *
+ * Jeśli wymagamy od repozytorium, aby wykonywał zapytania unikalne dla danej domeny, możemy posłużyć się deklaracjami metod w oparciu o kluczowe elementy sygnatury metody, tj.
+ *  - czasownik w języku ang. (find/read/get/count)
+ *  - opcjonalny podmiot (Order)
+ *  - słowo 'By'
+ *  - predykat
+ *
+ * Jeśli specyfika zapytania wymaga użycia bardziej skomplikowanej nazwy, możemy posłużyć się adnotacją @ Query, ktrej atrybutem jest zapytanie pobierające informację z bazy danych.
+ * Takiej metodzie możemy nadac dowolną nazwę. Przy pomocy tej adnotacji możemy wykonać dużo bardziej skomplikowane zapytania przy pomocy języka JPQL lub natywnego języka SQL
+ * (w tym celu należy ustawić wartość atrybutu nativeQuery = true).
  */
 
 @Repository
--- src/main/java/pizza/repositories/jpa/JpaOrderRepository.java
+++ src/main/java/pizza/repositories/jpa/JpaOrderRepository.java
@@ -1,8 +1,12 @@
 package pizza.repositories.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pizza.domain.Order;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * W przypadku implementacji interfejsów z JDBC należało zadbać o to, by zadeklarować metody realizujące operacje CRUD.
@@ -10,8 +14,25 @@ import pizza.domain.Order;
  * Rozszerzając nasz interfejs o sparametryzowany interfejs JpaRepository otrzymujemy gotowy zestaw metod ogólnego przeznaczenia trwałego magazynu danych dla encji.
  * Po uruchomieniu aplikacji Spring Data JPA samodzielnie generuje potrzebne implementacje.
  *
+ * Jeśli wymagamy od repozytorium, aby wykonywał zapytania unikalne dla danej domeny, możemy posłużyć się deklaracjami metod w oparciu o konwencję nazewniczą
+ * opartą o kluczowe elementy sygnatury metody, tj.
+ *  - czasownik w języku ang. (find/read/get/count)
+ *  - opcjonalny podmiot (Order)
+ *  - słowo 'By'
+ *  - predykat
+ *
+ * Jeśli specyfika zapytania wymaga użycia bardziej skomplikowanej nazwy, możemy posłużyć się adnotacją @ Query, ktrej atrybutem jest zapytanie pobierające informację z bazy danych.
+ * Takiej metodzie możemy nadac dowolną nazwę. Przy pomocy tej adnotacji możemy wykonać dużo bardziej skomplikowane zapytania przy pomocy języka JPQL lub natywnego języka SQL
+ * (w tym celu należy ustawić wartość atrybutu nativeQuery = true).
  */
 
 @Repository
 public interface JpaOrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findOrdersByNameOrderByOrderedAt(String name);
+    List<Order> getOrdersByPostCodeAndOrderedAtBetween(String postCode, Date startDate, Date endDate);
+    int countOrdersByName(String name);
+
+    @Query("Select o From Order o Where o.city = 'Warszawa'")
+    List<Order> readOrdersDeliveredInWarsaw();
 }
